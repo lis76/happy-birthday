e056c00f18539d13c2c44aaa187f9619ba790e25
lis76 <rsavoskin@yandex.ru>
2022-06-24 12:05:22 +0300

    Дизайн

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def landing
+  end
+  def rs
+  end
+  def lera
+  end
+  def se
+  end
+  def mama
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Happy
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.asset.precompile << "audios/*"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
