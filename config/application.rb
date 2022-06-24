7b788e5d572295829d69cc5f85f7053d7242f219
lis76 <rsavoskin@yandex.ru>
2022-06-24 12:48:12 +0300

    Установка Хероку3

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Happy
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.asset.precompile << "audios/*"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
